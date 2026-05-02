d6d5e0e9ddeb6e103a90857b25be802a1a14af7b
Joncy Keda <kedajoncy21@gmail.com>
2026-05-02 09:44:25 +0530

    Add SimpleModel class for basic linear regression

---
+++ model/model.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Linear(2, 1)
+
+    def forward(self, x):
+        return self.fc(x)
